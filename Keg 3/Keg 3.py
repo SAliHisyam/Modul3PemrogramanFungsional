73fd8861c24230697b70f2cca1788eca66d61803
SAliHisyam <127366847+SAliHisyam@users.noreply.github.com>
2023-10-29 09:43:09 +0700

    Update Keg 3.py

--- Keg 3/Keg 3.py
+++ Keg 3/Keg 3.py
@@ -1,11 +1,8 @@
 random_list = [105, 3.1, "Hello", 737, "Python", 2.7, "World", 412, 5.5, "AI"]
 
-# Memisahkan elemen-elemen berdasarkan jenis datanya menggunakan map() dan filter()
 data_float = list(filter(lambda x: isinstance(x, float), random_list))
 data_int = list(filter(lambda x: isinstance(x, int), random_list))
 data_string = list(filter(lambda x: isinstance(x, str), random_list))
-
-# Membuat fungsi untuk mengelompokkan angka dalam "Data Int" ke dalam ratusan, puluhan, dan satuan
 
 
 def categorize_int(number):
@@ -15,10 +12,8 @@ def categorize_int(number):
     return {'ratusan': ratusan, 'puluhan': puluhan, 'satuan': satuan}
 
 
-# Menggunakan map() untuk mengelompokkan angka dalam "Data Int"
 categorized_data_int = list(map(categorize_int, data_int))
 
-# Output sesuai dengan format yang diinginkan
 print("Data Float:", tuple(data_float))
 print("Data Int:")
 for item in categorized_data_int:
